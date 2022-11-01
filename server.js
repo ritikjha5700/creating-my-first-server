91b65d7c4c927f0d0444b483110787b6cae50d7e
kraken-23 <ritik065.hiteie2020@gmail.com>
2022-11-01 11:48:30 +0530

    completed my first node.js server

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const port = 9534;
+
+http
+  .createServer((request, response) => {
+    response.writeHead(200, { "content-Type": "text/html" });
+    response.write("<h1> Hello, this is from server </h1>");
+    response.end();
+  })
+  .listen(port, () => {
+    console.log(`node.js server started on port ${port}`);
+  });
+
+// http://localhost:9534
+// "_comment1": "start is a predefined keyword, so to run: (npm start) will work",
+// "_comment2": "ritik is not a predefined keyword, so to run: (npm run ritik) will work"
